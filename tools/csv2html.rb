6f06b0cebb1abe3941a3a52f3bdeca82e9fa217d
soboro <soboro@Ys-iMac.local>
2017-02-13 02:11:29 +0900

    [fix] begin using CSV class in csv2html.rb

--- tools/csv2html.rb
+++ tools/csv2html.rb
@@ -1,15 +1,18 @@
+require 'csv'
+
 #filename = ARGV[0]
 filename = "#{Dir.pwd}/dummy.csv"
 begin
-    file = File.open(filename)
-    file.each_line do |line|
-        puts "line : #{line}"
+    csv_data = CSV.read(filename, headers: true)
+    puts "[info] start reading CSV file."
+    csv_data.each do |data|
+        print "data : "
+        p csv_data.to_a
     end
-
 rescue => ex
     puts "[Error] : #{ex.message}"
 
 ensure
-    file.close
+
 end 
 
