872dc5c9e2f26561027b6b11cde9c5d6ef382a84
soboro <soboro@Ys-iMac.local>
2017-02-15 00:06:36 +0900

    [edit] Try to use CSV.table

--- tools/csv2html.rb
+++ tools/csv2html.rb
@@ -3,11 +3,15 @@ require 'csv'
 #filename = ARGV[0]
 filename = "#{Dir.pwd}/dummy.csv"
 begin
-    csv_data = CSV.read(filename, headers: true)
+    csv_data = CSV.table(filename)
     puts "[info] start reading CSV file."
+    p
+    puts "headers : #{csv_data.headers}"
+    p
     csv_data.each do |data|
         print "data : "
-        p csv_data.to_a
+        p data
+        p
     end
 rescue => ex
     puts "[Error] : #{ex.message}"
