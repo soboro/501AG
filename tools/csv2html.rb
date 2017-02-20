8724e02e8ab0bb41d9f61c05862cc22b1383a34b
soboro <soboro@Ys-iMac.local>
2017-02-21 08:30:11 +0900

    [edit] erase debug logs

--- tools/csv2html.rb
+++ tools/csv2html.rb
@@ -58,7 +58,7 @@ end
 def tagger_tr(str, cls)
     pretag = "<tr class=" + double_quater(cls) + ">"
     suftag = "</tr>"
-    dest_str = "\n" + "\t" + pretag + str + suftag
+    dest_str = pretag + str + suftag
 
     return dest_str
 end
@@ -66,7 +66,7 @@ end
 def tagger_td(str, cls)
     pretag = "<td class=" + double_quater(cls) + ">"
     suftag = "</td>"
-    dest_str = "\n" + "\t" + pretag + str + suftag
+    dest_str = pretag + str + suftag
 
     #puts
     #puts "dest_str : #{dest_str}"
@@ -75,10 +75,10 @@ def tagger_td(str, cls)
     return dest_str
 end
 
-def tagger_a(str, rul, target)
-    pretag = "<a href=" + double_quater(cls) + " target=" + double_quater(target) + ">"
+def tagger_a(str, url, target)
+    pretag = "<a href=" + double_quater(url) + " target=" + double_quater(target) + ">"
     suftag = "</a>"
-    dest_str = "\n" + pretag + str + suftag
+    dest_str = pretag + str + suftag
 
     return dest_str
 end
@@ -93,25 +93,26 @@ end
 
 def main()
     country_icon_urls = Hash.new
-    country_icon_urls["DE"] = "https://static.wixstatic.com/media/1eae9a_25a3791b295c42fda5a5a12b617f4923~mv2.png";
-    country_icon_urls["SU"] = "https://static.wixstatic.com/media/1eae9a_2bf2fadcad624ae0ba0b4d170533ac85~mv2.png";
-    country_icon_urls["US"] = "https://static.wixstatic.com/media/1eae9a_6316e8982ea9487a811e8fa21d2c7146~mv2.png";
-    country_icon_urls["JP"] = "https://static.wixstatic.com/media/1eae9a_c8c64785e1384fb7862c1a29b38a5ef0~mv2.png";
-    country_icon_urls["CN"] = "https://static.wixstatic.com/media/1eae9a_de77a92d9e314c19a22047f590302820~mv2.png";
-    country_icon_urls["GB"] = "https://static.wixstatic.com/media/1eae9a_8b8e121ef13946f6a5431fb73fe3c500~mv2.png";
-    country_icon_urls["FR"] = "https://static.wixstatic.com/media/1eae9a_5d41aabd031c490b8b0b526e96661213~mv2.png";
-    country_icon_urls["CZ"] = "https://static.wixstatic.com/media/1eae9a_c6c7426a36744b37ad42b0f2bfd61c10~mv2.png";
-    country_icon_urls["SE"] = "https://static.wixstatic.com/media/1eae9a_2291473f297541c09ea9f811a2326cfd~mv2.png";
+    country_icon_urls["DE"] = "https://static.wixstatic.com/media/1eae9a_25a3791b295c42fda5a5a12b617f4923~mv2.png"
+    country_icon_urls["SU"] = "https://static.wixstatic.com/media/1eae9a_2bf2fadcad624ae0ba0b4d170533ac85~mv2.png"
+    country_icon_urls["US"] = "https://static.wixstatic.com/media/1eae9a_6316e8982ea9487a811e8fa21d2c7146~mv2.png"
+    country_icon_urls["JP"] = "https://static.wixstatic.com/media/1eae9a_c8c64785e1384fb7862c1a29b38a5ef0~mv2.png"
+    country_icon_urls["CN"] = "https://static.wixstatic.com/media/1eae9a_de77a92d9e314c19a22047f590302820~mv2.png"
+    country_icon_urls["GB"] = "https://static.wixstatic.com/media/1eae9a_8b8e121ef13946f6a5431fb73fe3c500~mv2.png"
+    country_icon_urls["FR"] = "https://static.wixstatic.com/media/1eae9a_5d41aabd031c490b8b0b526e96661213~mv2.png"
+    country_icon_urls["CZ"] = "https://static.wixstatic.com/media/1eae9a_c6c7426a36744b37ad42b0f2bfd61c10~mv2.png"
+    country_icon_urls["SE"] = "https://static.wixstatic.com/media/1eae9a_2291473f297541c09ea9f811a2326cfd~mv2.png"
 
     type_icon_urls = Hash.new
 
-    type_icon_urls["LT"] = "https://static.wixstatic.com/media/1eae9a_f5831ad5e71847a1b1871d2c089e3252~mv2.png";
-    type_icon_urls["MT"] = "https://static.wixstatic.com/media/1eae9a_135cf9081f454905bb74bc3e82b684ee~mv2.png";
-    type_icon_urls["HT"] = "https://static.wixstatic.com/media/1eae9a_12e817c76f704caa98f6d61521c0182b~mv2.png";
-    type_icon_urls["TD"] = "https://static.wixstatic.com/media/1eae9a_dc1f62b383514d489c12b58e8ec68481~mv2.png";
-    type_icon_urls["SPG"] = "https://static.wixstatic.com/media/1eae9a_c459211fa515474fb164239436d7b0cd~mv2.png";
+    type_icon_urls["LT"] = "https://static.wixstatic.com/media/1eae9a_f5831ad5e71847a1b1871d2c089e3252~mv2.png"
+    type_icon_urls["MT"] = "https://static.wixstatic.com/media/1eae9a_135cf9081f454905bb74bc3e82b684ee~mv2.png"
+    type_icon_urls["HT"] = "https://static.wixstatic.com/media/1eae9a_12e817c76f704caa98f6d61521c0182b~mv2.png"
+    type_icon_urls["TD"] = "https://static.wixstatic.com/media/1eae9a_dc1f62b383514d489c12b58e8ec68481~mv2.png"
+    type_icon_urls["SPG"] = "https://static.wixstatic.com/media/1eae9a_c459211fa515474fb164239436d7b0cd~mv2.png"
 
-        
+    playbutton_icon_url = "https://static.wixstatic.com/media/1eae9a_36fe888fd8d442d78da0958b4e6c41d5~mv2.png"
+
     #filename = ARGV[0]
     filename = "#{Dir.pwd}/dummy.csv"
 
@@ -134,10 +135,9 @@ def main()
 
             # Tier
             data = row[:tier]
-            row_code_tier = tagger_td(data.to_s, "tier")
-            output_code = output_code + row_code_tier
-            print output_code
-            puts "\n--------"
+            row_code = tagger_td(data.to_s, "tier")
+            row_code = "\n" + "\t" + row_code
+            output_code = output_code + row_code
 
             # Country
             data = row[:country]
@@ -151,69 +151,68 @@ def main()
                 p "[Error] undefined country code detected."
             end
 
-            row_code_country = tagger_td(img_code, "country")
-            output_code = output_code + row_code_country
-            print output_code
-            puts "\n--------"
+            row_code = tagger_td(img_code, "country")
+            row_code = "\n" + "\t" + row_code
+            output_code = output_code + row_code
 
             # Vehicletype
             data = row[:vehicletype]
             icon_url = ""
-            img_code = ""
+            img_code_vehicletype = ""
 
             if type_icon_urls.has_key?(data)
                 icon_url = type_icon_urls[data]
-                img_code = tagger_img(icon_url, "")
+                img_code_vehicletype = tagger_img(icon_url, "")
             elsif
                 p "[Error] undefined vehicletype code detected."
             end
 
-            row_code_vehicletype = tagger_td(img_code, "vehicletype")
-            output_code = output_code + row_code_vehicletype
-            print output_code
-            puts "\n--------"
+            row_code = tagger_td(img_code_vehicletype, "vehicletype")
+            row_code = "\n" + "\t" + row_code
+            output_code = output_code + row_code
 
             # VihicleName
             data = row[:vehiclename]
-            row_code_viheclename = tagger_td(data, "vehiclename")
-            output_code = output_code + row_code_viheclename
-            print output_code
-            puts "\n--------"
+            row_code = tagger_td(data, "vehiclename")
+            row_code = "\n" + "\t" + row_code
+            output_code = output_code + row_code
 
             # Map
             data = row[:map]
-            row_code_map = tagger_td(data, "map")
-            output_code = output_code + row_code_map
-            print output_code
-            puts "\n--------"
+            row_code = tagger_td(data, "map")
+            row_code = "\n" + "\t" + row_code
+            output_code = output_code + row_code
 
             # Exp
             data = row[:exp]
-            row_code_exp = tagger_td(data.to_s, "exp")
-            output_code = output_code + row_code_exp
-            print output_code
-            puts "\n--------"
+            row_code = tagger_td(data.to_s, "exp")
+            row_code = "\n" + "\t" + row_code
+            output_code = output_code + row_code
 
             # Instructor
             data = row[:instructor]
-            row_code_instructor = tagger_td(data, "instructor")
-            output_code = output_code + row_code_instructor
-            print output_code
-            puts "\n--------"
+            row_code = tagger_td(data, "instructor")
+            row_code = "\n" + "\t" + row_code
+            output_code = output_code + row_code
 
             # Link
-            data = row[:link]
-            row_code_playbutton = tagger_td(data, "playbutton")
-            output_code = output_code + row_code_playbutton
-            print output_code
-            puts "\n--------"
+            movie_url = row[:link]
+            img_code_playbutton = tagger_img(playbutton_icon_url, "")
+            link_code_playbutton = tagger_a(img_code_playbutton, movie_url, "_blank")
+            row_code = tagger_td(link_code_playbutton, "playbutton")
+            row_code = "\n" + "\t" + row_code
+            output_code = output_code + row_code
 
             # Date
             data = row[:update]
-            row_code_update = tagger_td(data, "uptade")
-            output_code = output_code + row_code_update
+            row_code = tagger_td(data, "uptade")
+            row_code = "\n" + "\t" + row_code
+            output_code = output_code + row_code
+
+            output_code = output_code + "\n"
+            output_code = tagger_tr(output_code, "datarow")
+
             print output_code
-            puts "\n--------"
 
             puts
             puts "========"
